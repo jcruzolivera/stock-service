4b019bf07dd8aae43224e4651d70361f0a0c0b1f
jcruzolivera <jolivera317@gmail.com>
2024-11-04 18:45:41 -0300

    Agregar validacion de articulo

--- src/services/addStock.service.ts
+++ src/services/addStock.service.ts
@@ -1,7 +1,14 @@
 import MovStock from "./../models/movStock";
+import Stock from "./../models/stock";
 
 export const addArticleStock = async (articleId: string, quantity: number) => {
   try {
+    const stockFound = await Stock.findOne({ articleId });
+
+    if (!stockFound) {
+      return null;
+    }
+
     let movStock = await MovStock.create({
       articleId,
       movType: "INCR",
